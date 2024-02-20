5a2b8750feba6366d1f88e660301ba09f9827a56
rebeccalct <congtinglai@163.com>
2024-02-20 01:17:20 +0100

    initial-0126

---
+++ EVALUATION FUNCTIONS/RESEMBLANCE/dimensional_resemblance.py
@@ -0,0 +1,269 @@
+#import libraries
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from matplotlib import pyplot as plt
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.manifold import Isomap
+import  umap  # t-SNE
+
+
+def preprocess_data(data) :
+
+    """Preprocess the given dataset applying One-Hot encoding of categorical attributes and Standardization for numerical attributes.
+    
+    Parameters
+    ----------
+    data : pandas.core.frame.DataFrame
+        The dataframe to be preprocessed
+
+    Returns
+    -------
+    numpy.ndarray
+        a matrix with the preprocessed data
+    """
+
+    #get categorical attributes
+    categorical_vars = data.select_dtypes(include=['category'])
+
+    #label encoder each categorical attribute
+    for c in categorical_vars.columns.tolist() :
+        categorical_vars[c] = LabelEncoder().fit_transform(categorical_vars[c])
+
+    #one-hot encode categorical variables
+    onehot_encoder_x = OneHotEncoder()
+    x_cat = onehot_encoder_x.fit_transform(categorical_vars).toarray()
+
+    #standardize numerical variables
+    numerical_vars =  data.select_dtypes(include=['int64','float64'])
+    x_num = StandardScaler().fit_transform(numerical_vars)
+
+    #return the standardized numerical attributes stacked with the one-hot encoded categorical attributes
+    return np.column_stack((x_num, x_cat))
+
+
+def pca_transform(data_scaled, labels) :
+
+    """Compute the PCA transform from a scaled data.
+    
+    Parameters
+    ----------
+    data_scaled : numpy.ndarray
+        A matrix with the scaled data to transform
+
+    labels : numpy.ndarray
+        Labels to assign to the transformed data (0 for real and 1 for synthetic)
+
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        a dataframe with the transformed data labelled
+    """
+
+    #compute the PCA transform
+    pca_transform = PCA(n_components=2).fit_transform(data_scaled)
+
+    #append labels to the transformed data
+    pca = np.append(pca_transform, labels, axis=1)
+
+    #return a dataframe with the transformed data
+    return pd.DataFrame(data=pca, columns=['PC1','PC2','Label'])
+
+
+def isomap_transform(data_scaled, labels) :
+
+    """Compute the Isomap transform from a scaled data.
+    
+    Parameters
+    ----------
+    data_scaled : numpy.ndarray
+        A matrix with the scaled data to transform
+
+    labels : numpy.ndarray
+        Labels to assign to the transformed data (0 for real and 1 for synthetic)
+
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        a dataframe with the transformed data labelled
+    """
+
+    #compute the Isomap transform
+    iso_transform = Isomap(n_components=2).fit_transform(data_scaled)
+
+    #append labels to the transformed data
+    iso = np.append(iso_transform, labels, axis=1)
+
+    #return a dataframe with the transformed data
+    return pd.DataFrame(data=iso, columns=['PC1','PC2','Label'])
+
+
+def batch(iterable, n=1) :
+
+    """Create iterable batches from a dataframe.
+    
+    Parameters
+    ----------
+    iterable : numpy.ndarray
+        A matrix to be divided in batches
+
+    n : int
+        Length of the batches to create
+    """
+
+    #get length of the matrix to divide in batches
+    l = len(iterable)
+
+    #loop to divide the data into batches of length n
+    for ndx in range(0, l, n):
+        yield iterable[ndx:min(ndx + n, l)]
+
+
+def isomap_transform_on_batch(data_scaled, labels) :
+
+    """Compute the Isomap transform on batch from a scaled data.
+    
+    Parameters
+    ----------
+    data_scaled : numpy.ndarray
+        A matrix with the scaled data to transform
+
+    labels : numpy.ndarray
+        Labels to assign to the transformed data (0 for real and 1 for synthetic)
+
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        a dataframe with the transformed data labelled
+    """
+
+    #initialize dataframe to save the values of the transformation
+    iso_df = pd.DataFrame(columns=['PC1','PC2','Label'])
+        
+    #loop to iterate over all batches of data
+    for (b, y) in zip(batch(data_scaled,10000),batch(labels,10000)) :
+
+        #transform the batch of data
+        iso_transform = Isomap(n_components=2).fit_transform(b)
+
+        #append the labels of the data
+        iso = np.append(iso_transform, y, axis=1)
+
+        #append the transformation of the actual batch to the dataframe that contains the transformation of all the batches
+        iso = pd.DataFrame(data=iso, columns=['PC1','PC2','Label'])
+        iso_df = iso_df.append(iso, ignore_index=True)
+
+    #return a dataframe with the transformed data
+    return iso_df
+
+
+def dra_distance(real,synthetic) :
+
+    """Compute the proposed DRA distance, which is a distance metric that indicates the distance between two dimensionality dimension plots. The metric is the joint distance between the baricenters distance and spread distance.
+    
+    Parameters
+    ----------
+    real : pandas.core.frame.DataFrame
+        A dataframe with the dimensionality results (PCA or ISOMAP) of the real data.
+        
+    synthetic: pandas.core.frame.DataFrame
+        A dataframe with the dimensionality results (PCA or ISOMAP) of the synthetic data.
+
+    Returns
+    -------
+    numpy.float64
+        the computed DRA distance metric.
+    """
+    
+    #compute baricenters distance
+    bc_real=np.mean(real[['PC1','PC2']].values)
+    bc_synth=np.mean(synthetic[['PC1','PC2']].values)
+    dist_real_synth = np.linalg.norm(bc_real - bc_synth)
+    
+    #compute spread distance
+    spread_real=np.std(real[['PC1','PC2']].values)
+    spread_synth=np.std(synthetic[['PC1','PC2']].values)
+    dist_spread_real_synth = np.abs(spread_real-spread_synth)
+    
+    #compute joint distance
+    alpha=0.05
+    return np.round(alpha*dist_real_synth + (1-alpha)*dist_spread_real_synth,4)
+
+def umap_transform(data_scaled, labels):
+    """
+    Compute the UMAP transform from scaled data.
+    
+    Parameters
+    ----------
+    data_scaled : numpy.ndarray
+        A matrix with the scaled data to transform
+
+    labels : numpy.ndarray
+        Labels to assign to the transformed data (0 for real and 1 for synthetic)
+
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        A dataframe with the transformed data labelled
+    """
+
+    # Compute the UMAP transform
+    umap_transform = umap.UMAP(n_components=2).fit_transform(data_scaled)
+
+    # Append labels to the transformed data
+    umap_data = np.append(umap_transform, labels.reshape(-1, 1), axis=1)
+
+    # Return a dataframe with the transformed data
+    return pd.DataFrame(data=umap_data, columns=['Component 1', 'Component 2', 'Label'])
+
+
+
+def umap_transform_on_batch(data_scaled, labels):
+    """
+    Compute the UMAP transform on batch from a scaled data.
+    
+    Parameters
+    ----------
+    data_scaled : numpy.ndarray
+        A matrix with the scaled data to transform
+
+    labels : numpy.ndarray
+        Labels to assign to the transformed data (0 for real and 1 for synthetic)
+
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        a dataframe with the transformed data labelled
+    """
+
+    # Initialize dataframe to save the values of the transformation
+    umap_df = pd.DataFrame(columns=['Component 1', 'Component 2', 'Label'])
+    
+    # Function to generate batches - assuming it's defined or available elsewhere
+    def batch(iterable, n=1):
+        l = len(iterable)
+        for ndx in range(0, l, n):
+            yield iterable[ndx:min(ndx + n, l)]
+    
+    # Loop to iterate over all batches of data
+    for (b, y) in zip(batch(data_scaled, 10000), batch(labels, 10000)):
+
+        # Transform the batch of data
+        umap_transform = umap.UMAP(n_components=2).fit_transform(b)
+
+        # Append the labels of the data
+        umap_batch_data = np.append(umap_transform, y.reshape(-1, 1), axis=1)
+
+        # Append the transformation of the actual batch to the dataframe
+        umap_batch_df = pd.DataFrame(data=umap_batch_data, columns=['Component 1', 'Component 2', 'Label'])
+        umap_df = umap_df.append(umap_batch_df, ignore_index=True)
+
+    # Return a dataframe with the transformed data
+    return umap_df
---
+++ EVALUATION FUNCTIONS/RESEMBLANCE/univariate_resemblance.py
@@ -0,0 +1,276 @@
+#import libraries
+import warnings
+warnings.filterwarnings("ignore")
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from scipy import stats
+from math import sqrt
+from scipy.spatial import distance
+from scipy.stats import chi2_contingency
+from sklearn.preprocessing import MinMaxScaler
+import seaborn as sns
+
+    
+def student_t_tests(real, synthetic) :
+
+    """Performs Student T-tests to compare numerical attributes of real data and synthetic data.
+    
+    Parameters
+    ----------
+    real : pandas.core.frame.DataFrame
+        The real dataframe
+
+    synthetic : pandas.core.frame.DataFrame
+        The synthetic dataframe
+
+    Returns
+    -------
+    list
+        a list the p-values of the statistical tests
+    """
+
+    #get list of numerical column names
+    num_cols = (real.select_dtypes(include=['int64','float64'])).columns
+
+    #initialize a list to save the p-values of the tests
+    p_values = []
+
+    #loop to perform the tests for each attribute
+    for c in num_cols :
+        _, p = stats.ttest_ind(real[c], synthetic[c])
+        p_values.append(p)
+
+    #return the obtained p-values
+    return p_values
+
+
+def mann_whitney_tests(real, synthetic) :
+
+    """Performs Mann Whitney U-Tests to compare numerical attributes of real data and synthetic data.
+    
+    Parameters
+    ----------
+    real : pandas.core.frame.DataFrame
+        The real dataframe
+
+    synthetic : pandas.core.frame.DataFrame
+        The synthetic dataframe
+
+    Returns
+    -------
+    list
+        a list the p-values of the statistical tests
+    """
+
+    #get list of numerical column names
+    num_cols = (real.select_dtypes(include=['int64','float64'])).columns
+
+    #initialize a list to save the p-values of the tests
+    p_values = []
+
+    #loop to perform the tests for each attribute
+    for c in num_cols :
+        _, p = stats.mannwhitneyu(real[c], synthetic[c])
+        p_values.append(p)
+
+    #return the obtained p-values
+    return p_values
+
+
+def ks_tests(real, synthetic) :
+
+    """Performs Kolmogorov Smirnov tests to compare numerical attributes of real data and synthetic data.
+    
+    Parameters
+    ----------
+    real : pandas.core.frame.DataFrame
+        The real dataframe
+
+    synthetic : pandas.core.frame.DataFrame
+        The synthetic dataframe
+
+    Returns
+    -------
+    list
+        a list the p-values of the statistical tests
+    """
+
+    #get list of numerical column names
+    num_cols = (real.select_dtypes(include=['int64','float64'])).columns
+
+    #initialize a list to save the p-values of the tests
+    p_values = []
+
+    #loop to perform the tests for each attribute
+    for c in num_cols :
+        _, p = stats.ks_2samp(real[c], synthetic[c])
+        p_values.append(p)
+
+    #return the obtained p-values
+    return p_values
+
+
+def chi_squared_tests(real, synthetic) :
+
+    """Performs Chi-squared tests to compare categorical attributes of real data and synthetic data.
+    
+    Parameters
+    ----------
+    real : pandas.core.frame.DataFrame
+        The real dataframe
+
+    synthetic : pandas.core.frame.DataFrame
+        The synthetic dataframe
+
+    Returns
+    -------
+    list
+        a list the p-values of the statistical tests
+    """
+
+    #get list of categorical column names
+    cat_cols = (real.select_dtypes(include=['category'])).columns
+
+    #initialize a list to save the p-values of the tests
+    p_values = []
+
+    #loop to perform the tests for each attribute
+    for c in cat_cols :
+        #create contingency table
+        observed = pd.crosstab(real[c], synthetic[c])
+        #perform chi-squared test
+        _, p, _, _ = chi2_contingency(observed)
+        p_values.append(p)
+
+    #return the obtained p-values
+    return p_values
+
+
+def scale_data(df) :
+
+    """Scale a dataframe to get the values between 0 and 1. It returns the scaled dataframe.
+    
+    Parameters
+    ----------
+    df : pandas.core.frame.DataFrame
+        The dataframe to scale
+
+    Returns
+    -------
+    pandas.core.frame.DataFrame
+        A dataframe with the scaled data
+    """
+
+    #initialize and fit the scaler
+    scaler = MinMaxScaler()
+    scaled = scaler.fit_transform(df)
+
+    #return the scaled dataframe
+    return pd.DataFrame(scaled, columns=df.columns.tolist())
+
+
+def cosine_distances(real, synthetic) :
+
+    """Compute the cosine distances between real data attributes and synthetic data attributes independently.
+    
+    Parameters
+    ----------
+    real : pandas.core.frame.DataFrame
+        The real dataframe
+
+    synthetic : pandas.core.frame.DataFrame
+        The synthetic dataframe
+
+    Returns
+    -------
+    list
+        a list with the distances values
+    """
+
+    #get list of numerical column names
+    num_cols = (real.select_dtypes(include=['int64','float64'])).columns
+
+    #initialize a list to save the distances values
+    dists = []
+
+    #loop to perform the distances for each attribute
+    for c in num_cols :
+        dists.append(distance.cosine(real[c].values, synthetic[c].values))
+
+    #return the list with the computed distances
+    return dists
+
+
+def js_distances(real, synthetic) :
+
+    """Compute the Jenshen-Shannon distances between real data attributes and synthetic data attributes independently.
+    
+    Parameters
+    ----------
+    real : pandas.core.frame.DataFrame
+        The real dataframe
+
+    synthetic : pandas.core.frame.DataFrame
+        The synthetic dataframe
+
+    Returns
+    -------
+    list
+        a list with the distances values
+    """
+
+    #get list of numerical column names
+    num_cols = (real.select_dtypes(include=['int64','float64'])).columns
+
+    #initialize a list to save the distances values
+    dists = []
+
+    #loop to perform the distances for each attribute
+    for c in num_cols :
+        dists.append(distance.jensenshannon(stats.norm.pdf(real[c].values), stats.norm.pdf(synthetic[c].values)))
+
+    #return the list with the computed distances
+    return dists
+
+
+def wass_distances(real, synthetic) :
+
+    """Compute the Wasserstein distances between real data attributes and synthetic data attributes independently.
+    
+    Parameters
+    ----------
+    real : pandas.core.frame.DataFrame
+        The real dataframe
+
+    synthetic : pandas.core.frame.DataFrame
+        The synthetic dataframe
+
+    Returns
+    -------
+    list
+        a list with the distances values
+    """
+
+    #get list of numerical column names
+    num_cols = (real.select_dtypes(include=['int64','float64'])).columns
+
+    #initialize a list to save the distances values
+    dists = []
+
+    #loop to perform the distances for each attribute
+    for c in num_cols :
+        dists.append(stats.wasserstein_distance(real[c].values, synthetic[c].values))
+
+    #return the list with the computed distances
+    return dists
+
+
+
+def euclidean_distances(real, synthetic):
+    """Compute the Euclidean distances between real data attributes and synthetic data attributes independently."""
+    num_cols = real.select_dtypes(include=['int64', 'float64']).columns
+    dists = []
+    for c in num_cols:
+        dists.append(distance.euclidean(real[c].values, synthetic[c].values))
+    return dists
